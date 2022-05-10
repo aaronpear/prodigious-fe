75db0ad08a3febbe9f3ed20330d63f671370b86f
Aaron Reyes <56135909+aaronpear@users.noreply.github.com>
2022-05-10 16:23:58 -0700

    basic styling done

--- src/LoginPage/LoginPage.js
+++ src/LoginPage/LoginPage.js
@@ -4,31 +4,33 @@ import '../styles/loginPage.css';
 const LoginPage = () => {
     return (
         <div id="page-container">
-            <h2>Login To Your Account</h2>
-            <form>
-                <div class="form-group row">
-                    <label for="input-email" class="col-sm-2 col-form-label">Email</label>
-                    <div class="col-sm-10">
-                        <input type="email" class="form-control" id="input-email" placeholder="Email" />
+            <div id="main-container">
+                <h2>Login To Your Account</h2>
+                <form>
+                    <div class="form-group row">
+                        <label for="input-email" class="col-sm-2 col-form-label">Email</label>
+                        <div class="col-sm-10">
+                            <input type="email" class="form-control" id="input-email" placeholder="Email" />
+                        </div>
                     </div>
-                </div>
-                <div class="form-group row">
-                    <label for="input-password" class="col-sm-2 col-form-label">Password</label>
-                    <div class="col-sm-10">
-                        <input type="password" class="form-control" id="input-password" placeholder="Password" />
+                    <div class="form-group row">
+                        <label for="input-password" class="col-sm-2 col-form-label">Password</label>
+                        <div class="col-sm-10">
+                            <input type="password" class="form-control" id="input-password" placeholder="Password" />
+                        </div>
                     </div>
-                </div>
-                <div className="d-grid gap-2">
-                    <Button variant="primary" size="sm">
-                        Login
-                    </Button>
-                </div>
-            </form>
+                    <div className="d-grid gap-2">
+                        <Button variant="primary" size="sm" type ="submit" id="login-button">
+                            Login
+                        </Button>
+                    </div>
+                </form>
 
-            <p>Don't have an account?</p>
-            <Button variant="primary" size="sm">
-                Sign Up Here!
-            </Button>
+                <p>Don't have an account?</p>
+                <Button variant="outline-primary" size="sm" id="signup-button">
+                    Sign Up Here!
+                </Button>
+            </div>
         </div>
     );
 }
