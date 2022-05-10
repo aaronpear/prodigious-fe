828a8dc458d5475b9c5fd82b5820563e0f7d24ae
Aaron Reyes <56135909+aaronpear@users.noreply.github.com>
2022-05-10 15:35:54 -0700

    bit of header styling

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,17 +1,20 @@
 import { Link, useLinkClickHandler } from "react-router-dom";
 import { Button } from "react-bootstrap";
+import '../styles/header.css';
 
 const Header = () => {
     const handleClick = useLinkClickHandler("/login");
     return (
         <header>
-            <h1>Prodigious</h1>
+            <h1 class="title">Prodigious</h1>
             <nav>
                 <div class='links'>
-                    <Link to="/catalog">Card Catalog</Link>
-                    <Link to="/my-collection">My Collection</Link>
-                    <Link to="/deck-builder">Deck Builder</Link>
-                    <Button variant="primary" onClick={handleClick}>Log In</Button>
+                    <Link to="/catalog" class='link' >Card Catalog</Link>
+                    <Link to="/my-collection" class='link' >My Collection</Link>
+                    <Link to="/deck-builder" class='link' >Deck Builder</Link>
+                    <div id="login-button-container">
+                        <Button variant="primary" onClick={handleClick} id="login-button">Log In</Button>
+                    </div>
                 </div>    
             </nav>
         </header>
