7a74f3f34b09eff04f26b825f982fb4fea25b6dc
Aaron Reyes <56135909+aaronpear@users.noreply.github.com>
2022-05-20 14:33:05 -0700

    state and props set up

--- src/CardCatalog/FilterWindow.js
+++ src/CardCatalog/FilterWindow.js
@@ -1,4 +1,29 @@
-const FilterWindow = () => {
+import { useState } from 'react';
+
+const FilterWindow = (props) => {
+    const setCardData = props.setCardData;
+    const searchFormTemplate = {
+        "sort": "",
+        "limit": 0,
+        "offset": 0,
+        "filters": [
+          {
+            "name": "",
+            "values": []
+          }
+        ]
+     }
+    
+    // User checks sort/filter values in form ->
+    // we make API request with built searchForm ->
+    // set productIds to ids received from previous request (string separated only by commas) ->
+    // make another API request using product ids ->
+    // set cardData with previous API response
+
+    // *Will need to cache searches/results in the future for performance improvements
+    const [searchForm, setSearchForm] = useState(searchFormTemplate);
+    const [productIds, setProductIds] = useState([]);
+
     return (
         <h3>Filter</h3>
     )
