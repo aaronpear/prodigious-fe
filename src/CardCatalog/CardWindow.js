48967e073f3a693c154594e2ddc52a3cc72040da
Aaron Reyes <56135909+aaronpear@users.noreply.github.com>
2022-08-26 15:13:39 -0700

    added to last and to first page buttons for pagination

--- src/CardCatalog/CardWindow.js
+++ src/CardCatalog/CardWindow.js
@@ -7,6 +7,7 @@ const CardWindow = (props) => {
 
     // Need to refactor once search functionality is complete
     const [cardData, setCardData] = useState([]);
+    const [totalResults, setTotalResults] = useState(0);
     const [displayLimit, setDisplayLimit] = useState(50);
     const [displayOffset, setDisplayOffset] = useState(0);
 
@@ -14,17 +15,22 @@ const CardWindow = (props) => {
         axios.get(`https://prodigious-be.herokuapp.com/tcgPlayer/${displayLimit}/${displayOffset}`)
             .then((res) => {
                 setCardData(res.data.results);
+                setTotalResults(res.data.totalItems);
             })
             .catch(err => {
                 console.log(err);
             })}
     , [displayLimit, displayOffset]);
     
-    const nextPage = () => {
-        setDisplayOffset(displayOffset + 1);
+    const increaseOffset = () => {
+        // only increase offset if there exists more items to retrieve
+        if ((displayOffset + 1) * displayLimit < (totalResults)) {
+            setDisplayOffset(displayOffset + 1);
+        }
     }
 
-    const previousPage = () => {
+    const decreaseOffset = () => {
+        // do not decrease offset if we are at the beginning of the list
         if (displayOffset > 0) {
             setDisplayOffset(displayOffset - 1);
         }
@@ -38,15 +44,18 @@ const CardWindow = (props) => {
                     return <Card 
                         key={card.productId} 
                         cardName={card.cleanName} 
-                        imageUrl={card.imageUrl} 
+                        imageUrl={card.imageUrl}
                         data={card.extendedData}
                     />
                 })}
             </div>
             <div id="pagination-bar">
                 <Pagination>
-                    <Pagination.Prev onClick={previousPage} />
-                    <Pagination.Next onClick={nextPage} />
+                    <Pagination.First onClick={() => setDisplayOffset(0)}/>
+                    <Pagination.Prev onClick={decreaseOffset} />
+                    <Pagination.Item>Page {displayOffset + 1} of {(Math.ceil(totalResults / displayLimit))}</Pagination.Item>
+                    <Pagination.Next onClick={increaseOffset} />
+                    <Pagination.Last onClick={() => setDisplayOffset(Math.ceil(totalResults / displayLimit) - 1)}/>
                 </Pagination>
             </div>
         </div>
