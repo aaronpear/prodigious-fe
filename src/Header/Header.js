78ac5228f8949240e9e15d5193cecdc9c047978c
Aaron Reyes <56135909+aaronpear@users.noreply.github.com>
2022-11-04 15:31:04 -0700

    nav links now toggle between active and inactive state

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,17 +1,29 @@
-import { Link, useLinkClickHandler } from "react-router-dom";
+import { NavLink, useLinkClickHandler } from "react-router-dom";
 import { Button } from "react-bootstrap";
 import '../styles/header.css';
+import { getOverlayDirection } from "react-bootstrap/esm/helpers";
 
 const Header = () => {
     const handleClick = useLinkClickHandler("/login");
+
+    const activeStyle = {
+        color: "lightgrey",
+    }
+
     return (
         <header>
             <h1 className="title">Prodigious</h1>
             <nav>
                 <div className='links'>
-                    <Link to="/catalog" className='link' >Card Catalog</Link>
-                    <Link to="/my-collection" className='link' >My Collection</Link>
-                    <Link to="/deck-builder" className='link' >Deck Builder</Link>
+                    <NavLink to="/catalog" className='link' style={({ isActive }) => isActive ? activeStyle : undefined}>
+                        Card Catalog
+                    </NavLink>
+                    <NavLink to="/my-collection" className='link' style={({ isActive }) => isActive ? activeStyle : undefined}>
+                        My Collection
+                    </NavLink>
+                    <NavLink to="/deck-builder" className='link' style={({ isActive }) => isActive ? activeStyle : undefined}>
+                        Deck Builder
+                    </NavLink>
                     <div id="login-button-container">
                         <Button variant="primary" onClick={handleClick} id="login-button">Log In</Button>
                     </div>
