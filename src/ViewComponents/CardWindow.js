489b8f0f83ef30ec63a28d7a67a0d2b1ecab4eba
Aaron Reyes <56135909+aaronpear@users.noreply.github.com>
2022-11-04 19:03:55 -0700

    cleaned up displayLimit selector code

--- src/ViewComponents/CardWindow.js
+++ src/ViewComponents/CardWindow.js
@@ -78,11 +78,11 @@ const CardWindow = (props) => {
                             id="page-number"
                         />
                         <Form.Label>Cards Displayed Per Page:</Form.Label>
-                        <Form.Select size="sm" onChange={(e) => setDisplayLimit(parseInt(e.target.value))}>
-                            <option eventKey="1" value={25}>25</option>
-                            <option eventKey="2" value={50} selected>50</option>
-                            <option eventKey="3" value={75}>75</option>
-                            <option eventKey="4" value={100}>100</option>
+                        <Form.Select size="sm" defaultValue={50} onChange={(e) => setDisplayLimit(parseInt(e.target.value))}>
+                            <option value={25}>25</option>
+                            <option value={50}>50</option>
+                            <option value={75}>75</option>
+                            <option value={100}>100</option>
                         </Form.Select>
                     </div>
                 </div>
